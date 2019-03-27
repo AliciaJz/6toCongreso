7e57d76bdd22c382b80b00cd4fb77b3c13398877
AliciaJz <aliciajuarez.dev@gmail.com>
2019-03-26 20:08:58 -0600

    storage added

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,54 +1,6 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
-export const getAsitente = `query GetAsitente($id: ID!, $NombreGafete: String!) {
-  getAsitente(id: $id, NombreGafete: $NombreGafete) {
-    id
-    NombreGafete
-    NombreCertificado
-    Clases
-  }
-}
-`;
-export const listAsitentes = `query ListAsitentes(
-  $filter: TableAsitenteFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listAsitentes(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      NombreGafete
-      NombreCertificado
-      Clases
-    }
-    nextToken
-  }
-}
-`;
-export const getClase = `query GetClase($id: ID!, $NombreClase: String!) {
-  getClase(id: $id, NombreClase: $NombreClase) {
-    id
-    NombreClase
-    Ponente
-  }
-}
-`;
-export const listClases = `query ListClases(
-  $filter: TableClaseFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listClases(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
-      id
-      NombreClase
-      Ponente
-    }
-    nextToken
-  }
-}
-`;
 export const getBlog = `query GetBlog($id: ID!) {
   getBlog(id: $id) {
     id
@@ -153,6 +105,54 @@ export const listComments = `query ListComments(
         id
         title
       }
+    }
+    nextToken
+  }
+}
+`;
+export const getAsitente = `query GetAsitente($id: ID!) {
+  getAsitente(id: $id) {
+    id
+    NombreGafete
+    NombreCertificado
+    Clases
+  }
+}
+`;
+export const listAsitentes = `query ListAsitentes(
+  $filter: ModelAsitenteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listAsitentes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      NombreGafete
+      NombreCertificado
+      Clases
+    }
+    nextToken
+  }
+}
+`;
+export const getClase = `query GetClase($id: ID!) {
+  getClase(id: $id) {
+    id
+    NombreClase
+    Ponente
+  }
+}
+`;
+export const listClases = `query ListClases(
+  $filter: ModelClaseFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listClases(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      NombreClase
+      Ponente
     }
     nextToken
   }
